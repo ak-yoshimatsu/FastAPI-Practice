86b5eb5afb73132b1a4811282effdcdfaefd56f1
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2024-02-13 22:39:29 +0900

    add logger

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
+# from logging import StreamHandler, basicConfig, getLogger
+import logging
+
 from fastapi import FastAPI
 
 from app.s1.routes import users
 from app.s2.routes import mail, minio, patients
+
+logger = logging.getLogger("uvicorn")
+
+file_handler = logging.FileHandler("app.log")
+
+formatter = logging.Formatter("[%(levelname)s] %(asctime)s - %(message)s")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 
 @app.get("/")
 def index() -> dict[str, str]:
+    logging.info("retuning index INFO")
     return {"result": "index"}
 
 
